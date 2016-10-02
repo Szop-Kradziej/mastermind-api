31c2236a827a7a6a1a734656108c1034d5b91ab2
Szop-kradziej <karolina.drabarz@gmail.com>
2016-10-03 00:01:12 +0200

    Return zero matches for the worst possible guess

--- src/main/java/mastermind/Mastermind.kt
+++ src/main/java/mastermind/Mastermind.kt
@@ -3,4 +3,9 @@ package mastermind
 class Mastermind(val firstColor: Int,
                  val secondColor: Int,
                  val thirdColor: Int,
-                 val fourthColor: Int)
+                 val fourthColor: Int) {
+
+    fun  findMatch(i: Int, i1: Int, i2: Int, i3: Int): Int {
+        return 0;
+    }
+}
--- src/test/java/mastermind/MastermindTest.kt
+++ src/test/java/mastermind/MastermindTest.kt
@@ -8,4 +8,9 @@ class MastermindTest {
     fun shouldCreateMastermind() {
         Assert.assertNotNull(Mastermind(0,0,0,0))
     }
+
+    @Test
+    fun shouldReturnZeroMatchesForTheWorstPossibleGuessCorrectly() {
+        Assert.assertEquals(0, Mastermind(0,0,0,0).findMatch(1,1,1,1))
+    }
 }
