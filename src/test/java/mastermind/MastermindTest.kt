5b796056e8e7657961e0bc29e72d46dac873d4e5
Szop-kradziej <karolina.drabarz@gmail.com>
2016-10-03 00:35:34 +0200

    Add additional test cases

--- src/test/java/mastermind/MastermindTest.kt
+++ src/test/java/mastermind/MastermindTest.kt
@@ -20,5 +20,9 @@ class MastermindTest {
         Assert.assertEquals(1, Mastermind(0, 0, 0, 0).findMatch(1, 0, 1, 1))
         Assert.assertEquals(1, Mastermind(0, 0, 0, 0).findMatch(1, 1, 0, 1))
         Assert.assertEquals(1, Mastermind(0, 0, 0, 0).findMatch(1, 1, 1, 0))
+        Assert.assertEquals(1, Mastermind(0, 1, 1, 1).findMatch(0, 0, 0, 0))
+        Assert.assertEquals(1, Mastermind(1, 0, 1, 1).findMatch(0, 0, 0, 0))
+        Assert.assertEquals(1, Mastermind(1, 1, 0, 1).findMatch(0, 0, 0, 0))
+        Assert.assertEquals(1, Mastermind(1, 1, 1, 0).findMatch(0, 0, 0, 0))
     }
 }
