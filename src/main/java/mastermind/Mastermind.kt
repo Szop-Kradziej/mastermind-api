fcc9c5496da8d14288be17a83903795f745e6608
Szop-kradziej <karolina.drabarz@gmail.com>
2016-10-13 20:33:32 +0200

    [Refactor] Rename match functions

--- src/main/java/mastermind/Mastermind.kt
+++ src/main/java/mastermind/Mastermind.kt
@@ -5,17 +5,17 @@ class Mastermind(val firstColor: Int,
                  val thirdColor: Int,
                  val fourthColor: Int) {
 
-    fun findMatch(firstColorGuess: Int,
-                  secondColorGuess: Int,
-                  thirdColorGuess: Int,
-                  fourthColorGuess: Int): Int {
+    fun findPositionMatch(firstColorGuess: Int,
+                          secondColorGuess: Int,
+                          thirdColorGuess: Int,
+                          fourthColorGuess: Int): Int {
         return listOf(firstColor == firstColorGuess,
                 secondColor == secondColorGuess,
                 thirdColor == thirdColorGuess,
                 fourthColor == fourthColorGuess).count { it == true }
     }
 
-    fun findSwitchMatch(i: Int, i1: Int, i2: Int, i3: Int): Long {
+    fun findColorMatch(i: Int, i1: Int, i2: Int, i3: Int): Long {
         return 1;
     }
 }
--- src/test/java/mastermind/MastermindTest.kt
+++ src/test/java/mastermind/MastermindTest.kt
@@ -11,29 +11,29 @@ class MastermindTest {
 
     @Test
     fun shouldReturnZeroMatchesForTheWorstPossibleGuessCorrectly() {
-        Assert.assertEquals(0, Mastermind(0, 0, 0, 0).findMatch(1, 1, 1, 1))
-        Assert.assertEquals(0, Mastermind(1, 1, 1, 1).findMatch(0, 0, 0, 0))
+        Assert.assertEquals(0, Mastermind(0, 0, 0, 0).findPositionMatch(1, 1, 1, 1))
+        Assert.assertEquals(0, Mastermind(1, 1, 1, 1).findPositionMatch(0, 0, 0, 0))
     }
 
     @Test
-    fun shouldReturnOneMatchForOneCorrectColorGuess() {
-        Assert.assertEquals(1, Mastermind(0, 0, 0, 0).findMatch(0, 1, 1, 1))
-        Assert.assertEquals(1, Mastermind(0, 0, 0, 0).findMatch(1, 0, 1, 1))
-        Assert.assertEquals(1, Mastermind(0, 0, 0, 0).findMatch(1, 1, 0, 1))
-        Assert.assertEquals(1, Mastermind(0, 0, 0, 0).findMatch(1, 1, 1, 0))
-        Assert.assertEquals(1, Mastermind(0, 1, 1, 1).findMatch(0, 0, 0, 0))
-        Assert.assertEquals(1, Mastermind(1, 0, 1, 1).findMatch(0, 0, 0, 0))
-        Assert.assertEquals(1, Mastermind(1, 1, 0, 1).findMatch(0, 0, 0, 0))
-        Assert.assertEquals(1, Mastermind(1, 1, 1, 0).findMatch(0, 0, 0, 0))
+    fun shouldReturnOneMatchForOneCorrectPositionGuess() {
+        Assert.assertEquals(1, Mastermind(0, 0, 0, 0).findPositionMatch(0, 1, 1, 1))
+        Assert.assertEquals(1, Mastermind(0, 0, 0, 0).findPositionMatch(1, 0, 1, 1))
+        Assert.assertEquals(1, Mastermind(0, 0, 0, 0).findPositionMatch(1, 1, 0, 1))
+        Assert.assertEquals(1, Mastermind(0, 0, 0, 0).findPositionMatch(1, 1, 1, 0))
+        Assert.assertEquals(1, Mastermind(0, 1, 1, 1).findPositionMatch(0, 0, 0, 0))
+        Assert.assertEquals(1, Mastermind(1, 0, 1, 1).findPositionMatch(0, 0, 0, 0))
+        Assert.assertEquals(1, Mastermind(1, 1, 0, 1).findPositionMatch(0, 0, 0, 0))
+        Assert.assertEquals(1, Mastermind(1, 1, 1, 0).findPositionMatch(0, 0, 0, 0))
     }
 
     @Test
     fun shouldReturnPerfectMatchCorrectly() {
-        Assert.assertEquals(4, Mastermind(1, 2, 3, 4).findMatch(1, 2, 3, 4))
+        Assert.assertEquals(4, Mastermind(1, 2, 3, 4).findPositionMatch(1, 2, 3, 4))
     }
 
     @Test
-    fun shouldReturnSwitchPlacesMatchCorrectly() {
-        Assert.assertEquals(1, Mastermind(0, 0, 1, 0).findSwitchMatch(1, 2, 2, 2))
+    fun shouldReturnColorMatchCorrectly() {
+        Assert.assertEquals(1, Mastermind(0, 0, 1, 0).findColorMatch(1, 2, 2, 2))
     }
 }
