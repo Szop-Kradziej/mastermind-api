c9135279f52e77345694b0a0577e466c14bee0f4
Szop-kradziej <karolina.drabarz@gmail.com>
2016-10-13 21:31:43 +0200

    [Refactor] Extract findMatchResult method

--- src/test/java/mastermind/MastermindTest.kt
+++ src/test/java/mastermind/MastermindTest.kt
@@ -11,38 +11,50 @@ class MastermindTest {
 
     @Test
     fun shouldReturnZeroMatchesForTheWorstPossibleGuessCorrectly() {
-        Assert.assertEquals(0, Mastermind(0, 0, 0, 0).findMatch(1, 1, 1, 1).positionMatch)
-        Assert.assertEquals(0, Mastermind(1, 1, 1, 1).findMatch(0, 0, 0, 0).positionMatch)
+        Assert.assertEquals(0, findMatchResult(0, 0, 0, 0, 1, 1, 1, 1).positionMatch)
+        Assert.assertEquals(0, findMatchResult(1, 1, 1, 1, 0, 0, 0, 0).positionMatch)
     }
 
     @Test
     fun shouldReturnOneMatchForOneCorrectPositionGuess() {
-        Assert.assertEquals(1, Mastermind(0, 0, 0, 0).findMatch(0, 1, 1, 1).positionMatch)
-        Assert.assertEquals(1, Mastermind(0, 0, 0, 0).findMatch(1, 0, 1, 1).positionMatch)
-        Assert.assertEquals(1, Mastermind(0, 0, 0, 0).findMatch(1, 1, 0, 1).positionMatch)
-        Assert.assertEquals(1, Mastermind(0, 0, 0, 0).findMatch(1, 1, 1, 0).positionMatch)
-        Assert.assertEquals(1, Mastermind(0, 1, 1, 1).findMatch(0, 0, 0, 0).positionMatch)
-        Assert.assertEquals(1, Mastermind(1, 0, 1, 1).findMatch(0, 0, 0, 0).positionMatch)
-        Assert.assertEquals(1, Mastermind(1, 1, 0, 1).findMatch(0, 0, 0, 0).positionMatch)
-        Assert.assertEquals(1, Mastermind(1, 1, 1, 0).findMatch(0, 0, 0, 0).positionMatch)
+        Assert.assertEquals(1, findMatchResult(0, 0, 0, 0, 0, 1, 1, 1).positionMatch)
+        Assert.assertEquals(1, findMatchResult(0, 0, 0, 0, 1, 0, 1, 1).positionMatch)
+        Assert.assertEquals(1, findMatchResult(0, 0, 0, 0, 1, 1, 0, 1).positionMatch)
+        Assert.assertEquals(1, findMatchResult(0, 0, 0, 0, 1, 1, 1, 0).positionMatch)
+        Assert.assertEquals(1, findMatchResult(0, 1, 1, 1, 0, 0, 0, 0).positionMatch)
+        Assert.assertEquals(1, findMatchResult(1, 0, 1, 1, 0, 0, 0, 0).positionMatch)
+        Assert.assertEquals(1, findMatchResult(1, 1, 0, 1, 0, 0, 0, 0).positionMatch)
+        Assert.assertEquals(1, findMatchResult(1, 1, 1, 0, 0, 0, 0, 0).positionMatch)
     }
 
     @Test
     fun shouldReturnPerfectMatchCorrectly() {
-        Assert.assertEquals(4, Mastermind(1, 2, 3, 4).findMatch(1, 2, 3, 4).positionMatch)
+        Assert.assertEquals(4, findMatchResult(1, 2, 3, 4, 1, 2, 3, 4).positionMatch)
     }
 
     @Test
     fun shouldReturnColorMatchCorrectly() {
-        Assert.assertEquals(1, Mastermind(0, 0, 1, 0).findMatch(1, 2, 2, 2).colorMatch)
-        Assert.assertEquals(1, Mastermind(0, 0, 1, 1).findMatch(2, 1, 2, 1).colorMatch)
-        Assert.assertEquals(1, Mastermind(1, 1, 0, 1).findMatch(2, 2, 2, 0).colorMatch)
-        Assert.assertEquals(1, Mastermind(0, 0, 0, 1).findMatch(2, 1, 1, 2).colorMatch)
+        Assert.assertEquals(1, findMatchResult(0, 0, 1, 0, 1, 2, 2, 2).colorMatch)
+        Assert.assertEquals(1, findMatchResult(0, 0, 1, 1, 2, 1, 2, 1).colorMatch)
+        Assert.assertEquals(1, findMatchResult(1, 1, 0, 1, 2, 2, 2, 0).colorMatch)
+        Assert.assertEquals(1, findMatchResult(0, 0, 0, 1, 2, 1, 1, 2).colorMatch)
     }
 
     @Test
     fun shouldReturnConcreteStructureAsMatchResult() {
-        Assert.assertEquals(MatchResult(2, 2), Mastermind(0, 0, 1, 1).findMatch(0, 1, 0, 1))
+        Assert.assertEquals(MatchResult(2, 2), findMatchResult(0, 0, 1, 1, 0, 1, 0, 1))
+    }
+
+    private fun findMatchResult(firstColor: Int,
+                                secondColor: Int, 
+                                thirdColor: Int,
+                                fourthColor: Int,
+                                firstColorGuess: Int,
+                                secondColorGuess: Int,
+                                thirdColorGuess: Int,
+                                fourthColorGuess: Int): MatchResult {
+        return Mastermind(firstColor, secondColor, thirdColor, fourthColor)
+                .findMatch(firstColorGuess, secondColorGuess, thirdColorGuess, fourthColorGuess)
     }
 }
 
