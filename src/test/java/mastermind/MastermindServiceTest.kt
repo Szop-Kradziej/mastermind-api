eab35a7812458fa515a54d4332de81902601e07b
Szop-kradziej <karolina.drabarz@gmail.com>
2016-10-25 22:09:26 +0200

    Add empty MastermindService

---
+++ src/test/java/mastermind/MastermindServiceTest.kt
@@ -0,0 +1,15 @@
+package mastermind
+
+import org.junit.Assert
+import org.junit.Test
+
+class MastermindServiceTest {
+    @Test
+    fun shouldMastermindServiceExists() {
+        Assert.assertNotNull(MastermindService())
+    }
+}
+
+class MastermindService {
+
+}
