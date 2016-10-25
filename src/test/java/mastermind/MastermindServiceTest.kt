1aff27e50d6bf94fab5259a8744d4c05ebe28c92
Szop-kradziej <karolina.drabarz@gmail.com>
2016-10-26 00:08:45 +0200

    Add adding moves to game of selected id

--- src/test/java/mastermind/MastermindServiceTest.kt
+++ src/test/java/mastermind/MastermindServiceTest.kt
@@ -10,7 +10,7 @@ class MastermindServiceTest {
     fun shouldMovesContainsFirstGuess() {
         val mastermind = Mastermind(ColorSet(0, 1, 1, 1))
         val guess = listOf(0, 0, 0, 0)
-        Assert.assertEquals(guess, MastermindService({ mastermind }).makeMove(guess).moves.first().guess)
+        Assert.assertEquals(guess, MastermindService({ mastermind }).makeMove("id", guess).moves.first().guess)
     }
 
     @Test
@@ -18,7 +18,7 @@ class MastermindServiceTest {
         val mastermind = Mastermind(ColorSet(0, 1, 1, 1))
         val guess = listOf(0, 0, 0, 0)
         val matchResult = MatchResult(1, 0)
-        Assert.assertEquals(matchResult, MastermindService({ mastermind }).makeMove(guess).moves.first().match)
+        Assert.assertEquals(matchResult, MastermindService({ mastermind }).makeMove("id", guess).moves.first().match)
     }
 
     @Test
@@ -26,7 +26,7 @@ class MastermindServiceTest {
         val mastermind = Mastermind(ColorSet(0, 0, 1, 1))
         val guess = listOf(0, 0, 0, 0)
         val match = MatchResult(2, 0)
-        Assert.assertEquals(match, MastermindService({ mastermind }).makeMove(guess).moves.first().match)
+        Assert.assertEquals(match, MastermindService({ mastermind }).makeMove("id", guess).moves.first().match)
     }
 
     @Test
@@ -34,18 +34,38 @@ class MastermindServiceTest {
         val mastermind = Mastermind(ColorSet(0, 0, 0, 1))
         val guess = listOf(0, 0, 0, 0)
         val service = MastermindService({ mastermind })
-        service.makeMove(guess)
-        Assert.assertEquals(2, service.makeMove(guess).moves.size)
+        service.makeMove("id", guess)
+        Assert.assertEquals(2, service.makeMove("id", guess).moves.size)
+    }
+
+    @Test
+    fun shouldMovesContainsMovesOnlyForSpecifyGame() {
+        val mastermind = Mastermind(ColorSet(0, 0, 0, 1))
+        val guess = listOf(0, 0, 0, 0)
+        val service = MastermindService({ mastermind })
+        service.makeMove("id", guess)
+        Assert.assertEquals(1, service.makeMove("id2", guess).moves.size)
     }
 }
 
 class MastermindService(val randomGameGenerator: () -> Mastermind) {
-    val moves = ArrayList<Move>()
+    val games = HashMap<String, GameStatus>()
 
-    fun makeMove(guess: List<Int>): GameStatus {
+    fun makeMove(id: String, guess: List<Int>): GameStatus {
         val match = randomGameGenerator().findMatch(ColorSet.from(guess))
-        moves.add(Move(guess, match))
-        return GameStatus(moves)
+        addMoveToProperGame(id, Move(guess, match));
+        return games.get(id)!!
+    }
+
+    private fun  addMoveToProperGame(id: String, move: Move) {
+        if(games.containsKey(id)) {
+            val game = games.get(id)
+            val moves = game!!.moves.toMutableList()
+            moves.add(move)
+            games.put(id, GameStatus(moves))
+        } else {
+            games.put(id, GameStatus(listOf(move)))
+        }
     }
 }
 
