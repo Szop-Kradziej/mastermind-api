6faa47dde41d15301fe82ae8800597fd4ac8974d
Szop-kradziej <karolina.drabarz@gmail.com>
2016-10-26 19:33:14 +0200

    Add missing test for RandomGameGenerator

--- src/test/java/mastermind/RandomGameGeneratorTest.kt
+++ src/test/java/mastermind/RandomGameGeneratorTest.kt
@@ -3,7 +3,6 @@ package mastermind
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.whenever
 import org.junit.Assert
-import org.junit.Ignore
 import org.junit.Test
 
 class RandomGameGeneratorTest {
@@ -21,10 +20,17 @@ class RandomGameGeneratorTest {
         whenever(random.invoke()).thenReturn(6).thenReturn(7).thenReturn(8).thenReturn(9)
         Assert.assertEquals(Mastermind(ColorSet(6, 7, 0, 1)), RandomGameGenerator(random).invoke())
     }
+
+    @Test
+    fun shouldGenerateRandomGameWithFixMaxColorValueSecondCase() {
+        val random = mock<Function0<Int>>()
+        whenever(random.invoke()).thenReturn(8).thenReturn(9).thenReturn(3).thenReturn(9)
+        Assert.assertEquals(Mastermind(ColorSet(0, 1, 3, 1)), RandomGameGenerator(random).invoke())
+    }
 }
 
 class RandomGameGenerator(val random: () -> Int) : () -> Mastermind {
     override fun invoke(): Mastermind {
-        return Mastermind(ColorSet(random(), random(), random() % 8, random() % 8))
+        return Mastermind(ColorSet(random() % 8, random() % 8, random() % 8, random() % 8))
     }
 }
