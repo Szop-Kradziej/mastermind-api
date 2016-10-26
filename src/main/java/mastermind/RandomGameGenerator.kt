17c54dd8511970ebfa8eec82b634933a4ef12f6e
Szop-kradziej <karolina.drabarz@gmail.com>
2016-10-26 22:14:02 +0200

    [Fix] Fix problem with negative numbers in random game generator

--- src/main/java/mastermind/RandomGameGenerator.kt
+++ src/main/java/mastermind/RandomGameGenerator.kt
@@ -2,6 +2,8 @@ package mastermind
 
 class RandomGameGenerator(val random: () -> Int) : () -> Mastermind {
     override fun invoke(): Mastermind {
-        return Mastermind(ColorSet(random() % 8, random() % 8, random() % 8, random() % 8))
+        return Mastermind(ColorSet(getRandomColor(), getRandomColor(), getRandomColor(), getRandomColor()))
     }
+
+    private fun getRandomColor() = ((random() % 8) + 8) % 8
 }
--- src/test/java/mastermind/RandomGameGeneratorTest.kt
+++ src/test/java/mastermind/RandomGameGeneratorTest.kt
@@ -27,4 +27,11 @@ class RandomGameGeneratorTest {
         whenever(random.invoke()).thenReturn(8).thenReturn(9).thenReturn(3).thenReturn(9)
         Assert.assertEquals(Mastermind(ColorSet(0, 1, 3, 1)), RandomGameGenerator(random).invoke())
     }
+
+    @Test
+    fun shouldConvertNegativeNumbersToProperPositiveNumbers() {
+        val random = mock<Function0<Int>>()
+        whenever(random.invoke()).thenReturn(-1).thenReturn(-9).thenReturn(-7).thenReturn(-8)
+        Assert.assertEquals(Mastermind(ColorSet(7, 7, 1, 0)), RandomGameGenerator(random).invoke())
+    }
 }
