b2da04811d101ef1828c6564abff4a154fe09ce5
Szop-kradziej <karolina.drabarz@gmail.com>
2016-11-27 18:15:57 +0100

    Add delay between on and off

--- src/test/java/mastermind/MastermindRaspberryServiceTest.kt
+++ src/test/java/mastermind/MastermindRaspberryServiceTest.kt
@@ -1,30 +1,47 @@
 package mastermind
 
 import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.never
 import com.nhaarman.mockito_kotlin.verify
 import com.pi4j.io.gpio.GpioPinDigitalOutput
+import io.reactivex.Scheduler
+import io.reactivex.schedulers.TestScheduler
 import org.assertj.core.api.Assertions
 import org.junit.Test
+import java.util.concurrent.TimeUnit
 
 class MastermindRaspberryServiceTest {
 
     @Test
     fun shouldCreateMastermindRaspberryService() {
-        Assertions.assertThat(MastermindRaspberryService(mock<GpioPinDigitalOutput>())).isNotNull()
+        Assertions.assertThat(MastermindRaspberryService(mock(), TestScheduler())).isNotNull()
     }
 
     @Test
     fun shouldBlinkOnGpioController() {
         val output = mock<GpioPinDigitalOutput>()
-        MastermindRaspberryService(output).blink()
+        val scheduler = TestScheduler()
+        MastermindRaspberryService(output, scheduler).blink()
         verify(output).high()
+        scheduler.advanceTimeBy(1, TimeUnit.SECONDS)
         verify(output).low()
+    }
+
+    @Test
+    fun shouldTurnOnGpioController() {
+        val output = mock<GpioPinDigitalOutput>()
+        val scheduler = TestScheduler()
+        MastermindRaspberryService(output, scheduler).blink()
+        verify(output).high()
+        verify(output, never()).low()
     }
 }
 
-class MastermindRaspberryService(private val output: GpioPinDigitalOutput) {
+class MastermindRaspberryService(private val output: GpioPinDigitalOutput, private val scheduler: Scheduler) {
     fun blink() {
         output.high()
-        output.low()
+        scheduler.scheduleDirect({
+            output.low()
+        },1, TimeUnit.SECONDS)
     }
 }
