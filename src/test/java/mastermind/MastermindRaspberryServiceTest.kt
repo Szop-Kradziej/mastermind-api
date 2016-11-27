0ed44c0787e5a2840cf04d8481c8259076ac424f
Szop-kradziej <karolina.drabarz@gmail.com>
2016-11-27 17:07:43 +0100

    Add blink method

--- src/test/java/mastermind/MastermindRaspberryServiceTest.kt
+++ src/test/java/mastermind/MastermindRaspberryServiceTest.kt
@@ -1,5 +1,8 @@
 package mastermind
 
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import com.pi4j.io.gpio.GpioPinDigitalOutput
 import org.assertj.core.api.Assertions
 import org.junit.Test
 
@@ -7,10 +10,21 @@ class MastermindRaspberryServiceTest {
 
     @Test
     fun shouldCreateMastermindRaspberryService() {
-        Assertions.assertThat(MastermindRaspberryService()).isNotNull()
+        Assertions.assertThat(MastermindRaspberryService(mock<GpioPinDigitalOutput>())).isNotNull()
+    }
+
+    @Test
+    fun shouldBlinkOnGpioController() {
+        val output = mock<GpioPinDigitalOutput>()
+        MastermindRaspberryService(output).blink()
+        verify(output).high()
+        verify(output).low()
     }
 }
 
-class MastermindRaspberryService {
-
+class MastermindRaspberryService(private val output: GpioPinDigitalOutput) {
+    fun blink() {
+        output.high()
+        output.low()
+    }
 }
